6b1d1d69bc8e6f4a65845381896fc8a26f9fa3f9
Restaurante Casablanca <tmania762@gmail.com>
2026-09-18 05:25:00 +0200

    Delete js/app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    const year = document.getElementById("year");
-
-    if (year) {
-        year.textContent = new Date().getFullYear();
-    }
-
-});
